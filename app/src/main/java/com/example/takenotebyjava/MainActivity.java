bdf2d17367b5f543835b05421973514ad45091df
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2020-11-17 00:01:22 +0600

    implements ViewModel to MainActivity

--- app/src/main/java/com/example/takenotebyjava/MainActivity.java
+++ app/src/main/java/com/example/takenotebyjava/MainActivity.java
@@ -2,19 +2,36 @@ package com.example.takenotebyjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.LayoutInflaterCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 
 import com.example.takenotebyjava.databinding.ActivityMainBinding;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+
+    private NoteViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        viewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        viewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(getApplicationContext(), "onChanged", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
